04094885cf83a4b570b7cfc197494b1a89071cdb
JohnnyLui0405 <johnnylui2017@gmail.com>
2023-03-09 10:42:02 +0000

    V1

---
+++ my-vue-app/src/views/SignIn.vue
@@ -0,0 +1,21 @@
+<template>
+    <n-radio-group
+      v-model:value="size"
+      name="left-size"
+      style="margin-bottom: 12px"
+    >
+      <n-radio-button value="small">
+        Small
+      </n-radio-button>
+      <n-radio-button value="medium">
+        Medium
+      </n-radio-button>
+      <n-radio-button value="large">
+        Large
+      </n-radio-button>
+    </n-radio-group>
+    
+  </template>
+
+<script>
+</script>
